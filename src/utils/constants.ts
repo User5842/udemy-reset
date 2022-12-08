4b92b4219743fbe2037d521662f6e2da2623182e
Rafael Negron <r.negron1@umiami.edu>
2022-12-08 18:49:00 -0500

    fix(constants): refactor `udemyPage.lessons` selector
    fix #1

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,7 +5,7 @@ export const udemyPage = {
   checkbox: "[data-type='checkbox']",
   checked: "data-checked",
   courseContent: "[data-purpose='curriculum-section-container']",
-  lessons: ".unstyled-list",
+  lessons: ".ud-unstyled-list",
   lessonProgress: "[data-purpose='progress-toggle-button']",
   section: "[data-css-toggle-id]",
 };
